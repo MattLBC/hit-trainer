c40f4e56510353c540b35d7981fdafe115c60e6d
Matthew <matthew@Matthews-MacBook-Pro.local>
2023-03-03 19:03:56 +0000

    Minimal changes for HIT timer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
+import stopwatch from './logos/stopwatch-1.png'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={stopwatch} className="App-logo" alt="logo" />
+      <h1 className="App-header"> HIT timer </h1>
+      
     </div>
   );
 }
