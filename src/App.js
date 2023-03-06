f7ea6c286ce06d27effbd9636dc526d89c5d18f0
Matthew <matthew@Matthews-MacBook-Pro.local>
2023-03-06 16:32:31 +0000

    Puts time manipulation functions in seperate file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import stopwatch from './logos/stopwatch-1.png'
 import './App.css';
+import {getSecondsFromMMSS, toMMSS} from "./helper-functions/time-convert"
 
 function App() {
   const [prepTime, setPrepTime] = useState("00:00");
@@ -31,34 +32,6 @@ function App() {
 
     const time = toMMSS(seconds);
     setTime(time);
-  };
-
-  const getSecondsFromMMSS = (value) => {
-    const [str1, str2] = value.split(":");
-
-    const val1 = Number(str1);
-    const val2 = Number(str2);
-
-    if (!isNaN(val1) && isNaN(val2)) {
-      return val1;
-    }
-
-    if (!isNaN(val1) && !isNaN(val2)) {
-      return val1 * 60 + val2;
-    }
-    return 0;
-  };
-
-  const toMMSS = (secs) => {
-    const secNum = parseInt(secs.toString(), 10);
-    const minutes = Math.floor(secNum / 60);
-    const seconds = secNum % 60;
-
-    return [minutes, seconds]
-      .map((val) => (val < 10 ? `0${val}` : val))
-      .filter((val, index) => val !== "00" || index > 0)
-      .join(":")
-      .replace(/^0/, "");
   };
 
   return (
