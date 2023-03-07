82e7138c0ce2c110e0762da713de1903cc48e725
Matthew <matthew@conolly.uk>
2023-03-07 23:39:02 +0000

    Fixes issues with multiple intervals triggering, prevents timer from going past zero

--- src/components/timer.js
+++ src/components/timer.js
@@ -1,18 +1,17 @@
 import { useState, useEffect } from "react";
 
 function Timer() {
-  let time = 40;
+  const [time, setTime] = useState(5);
 
-  function countdown() {
-    if (time === 0) {
-      return;
-    }
-    time = time - 1;
-
-    console.log(time);
-  }
-
-  setInterval(countdown, 1000);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (time === 0) {
+        return;
+      }
+      setTime((time) => time - 1);
+    }, 1000);
+    return () => clearInterval(interval);
+  }, [time]);
 
   return (
     <>
